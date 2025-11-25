90343e77161b37b01553b824d8f23385a4821b6b
Jussi Nevavuori <jussi@nevavuori.fi>
2025-11-25 12:21:25 +0200

    feat: upgrade deps

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { requestId } from "hono/request-id";
+import { getConfig } from "./config/getConfig";
 import { mcpRouter } from "./routers/mcp.router";
 
 const app = new Hono();
@@ -12,4 +13,14 @@ app.use("*", requestId());
 
 app.route("/mcp", mcpRouter);
 
-export default Object.assign(app, { port: process.env.AIGRICOLA_PORT || 3031 });
+const port = process.env.AIGRICOLA_PORT || 3031;
+
+getConfig().then((config) => {
+  console.clear();
+  console.log(`✅ Aigricola running on port ${port}`);
+  console.log();
+  console.log(`⚙️  Using configuration`);
+  console.dir(config);
+});
+
+export default Object.assign(app, { port });
